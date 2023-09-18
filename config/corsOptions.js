a527463fd3c28e32109dd1c48399af596abbbebf
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-18 11:47:44 -0600

    added comment

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        //!==-1 denies access to any thing not lited in array, !origin will allow to use Postman or other tools
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
