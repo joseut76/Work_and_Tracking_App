6ba4ebb89ac9e33419a96b0d7a81ad40b20f2e5d
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-18 21:08:05 -0600

    add mongoose

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config() 
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -8,6 +9,7 @@ const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
+console.log(process.env.NODE_ENV)
 
 //middleware
 app.use(cors(corsOptions))
